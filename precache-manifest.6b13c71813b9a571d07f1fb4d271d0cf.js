8c3380db73bac26520379a90b1b429a81402faa9
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-25 09:38:09 +0530

    Updates

---
+++ precache-manifest.6b13c71813b9a571d07f1fb4d271d0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78f2531d7bf78a0c8f46a1fe3153596",
+    "url": "/Youtubeplayer/index.html"
+  },
+  {
+    "revision": "d7b25965b0648d2476c3",
+    "url": "/Youtubeplayer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32d55abc2a94ad707dad",
+    "url": "/Youtubeplayer/static/js/2.9a7a5cc4.chunk.js"
+  },
+  {
+    "revision": "1cf4077b186339249e244f8aa69b4e3e",
+    "url": "/Youtubeplayer/static/js/2.9a7a5cc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b25965b0648d2476c3",
+    "url": "/Youtubeplayer/static/js/main.6ad565eb.chunk.js"
+  },
+  {
+    "revision": "abf95938055d48feeb13",
+    "url": "/Youtubeplayer/static/js/runtime-main.b86a3f12.js"
+  }
+]);
